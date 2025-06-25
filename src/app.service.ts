1e746a58eaf71bf1c4a89214e892bcc0f506c78b
BurrTow <186544451+BurrTow@users.noreply.github.com>
2025-06-25 05:35:41 +0000

    feat: removed 2nd name from name on the spec file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Roberto Nicholas M. Sebastian';
+    return 'Roberto M. Sebastian';
   }
 }
