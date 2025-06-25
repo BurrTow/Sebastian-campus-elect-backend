4e69604be8a47ddb3510d646ae584e1f1b83a0a5
BurrTow <186544451+BurrTow@users.noreply.github.com>
2025-06-25 05:20:57 +0000

    feat: fixes

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Roberto M. Sebastian"', () => {
+      expect(appController.getHello()).toBe('Roberto M. Sebastian');
     });
   });
 });
